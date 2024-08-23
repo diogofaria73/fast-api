c2af7680a866ba2913a7bb463e7ddf107b2ac2cc
Diogo Faria <diogofaria073@gmail.com>
2024-08-23 13:44:03 -0300

    Add dockercompose file to create postgresql database

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from enum import Enum
 from fastapi import FastAPI
-from shared.infrastructure import http_global
+from shared.infrastructure import http_global as http_requests
 
 app = FastAPI()
 
-for element in http_global.http_list:
+for element in http_requests.routes_list:
     app.include_router(element)
